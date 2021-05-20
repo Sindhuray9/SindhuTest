2821c490d95330cbdfc1747cef01acedb9eea18d
Sindhura Yalavarthi <sindhuy98@gmail.com>
2021-05-20 11:52:46 -0400

    first commit

---
+++ APIConsume/APIConsume/Controllers/UserController.cs
@@ -0,0 +1,154 @@
+﻿using APIConsume.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace APIConsume.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+
+            IEnumerable<userViewModal> user = null;
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:52811/api/user");
+                //HTTP GET
+                var responseTask = client.GetAsync("user");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<IList<userViewModal>>();
+                    readTask.Wait();
+                    user = readTask.Result;
+                }
+                else //web api sent error response 
+                {                    
+                    user = Enumerable.Empty<userViewModal>();
+                    ModelState.AddModelError(string.Empty, "Server error. Please contact administrator.");
+                }
+            }
+            return View(user);
+
+         }
+
+        public ActionResult create()
+        {
+            return View();
+        }
+
+        [HttpPost]       
+        public ActionResult create(userViewModal user)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:52811/api/user");
+
+                //HTTP POST
+                var post = client.PostAsJsonAsync<userViewModal>("user", user);
+                post.Wait();
+
+                var result = post.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+
+            ModelState.AddModelError(string.Empty, "Server Error. Please contact administrator.");
+
+            return View(user);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            userViewModal user = null;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:52811/api/");
+
+                var responseTask = client.GetAsync("user/" + id.ToString());
+                responseTask.Wait();
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<userViewModal>();
+                    readTask.Wait();
+                    user = readTask.Result;
+                }
+                //HTTP POST
+               
+            }
+           
+            return View(user);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(userViewModal user)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:52811/api/user");
+
+                //HTTP POST
+                var put = client.PutAsJsonAsync<userViewModal>("user", user);
+                put.Wait();
+
+                var result = put.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return View(user);
+                }
+                
+            }
+
+           
+        }
+
+
+        
+        public ActionResult Delete(int id)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:52811/api/");
+
+                //HTTP DELETE
+                var deleteTask = client.DeleteAsync("user/" + id.ToString());
+                deleteTask.Wait();
+
+                var result = deleteTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+
+                    return RedirectToAction("Index");
+                }
+            }
+
+            return RedirectToAction("Index");
+        }
+
+
+
+
+
+    }
+
+
+
+
+
+
+}
---
+++ MyAPI/MyAPI/Controllers/UserController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using MyDBA;
+
+
+namespace MyAPI.Controllers
+{
+    public class UserController : ApiController
+    {
+
+        public IHttpActionResult Get()
+        {
+            try
+            {
+                using (ABCompanyEntities entities = new ABCompanyEntities())
+                {
+                    entities.Configuration.ProxyCreationEnabled = false;
+
+                    var entity = entities.tblUsers.ToList();
+                    if (entity != null && entity.Count>0)
+                    {
+                        return Ok(entity);
+                     
+                    }
+                    else
+                    {
+                        return Ok<string> ("No record in table");
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
+          
+        }
+
+        public IHttpActionResult Get(int id)
+        {
+            using (ABCompanyEntities entities = new ABCompanyEntities())
+            {
+                entities.Configuration.ProxyCreationEnabled = false;
+                try
+                {
+                  var entity = entities.tblUsers.FirstOrDefault(p => p.ID == id);
+                    if (entity != null)
+                    {
+                        return Ok(entity);
+
+                    }
+                    else
+                    {
+                        return Ok<string>("No record in table for id " + id);
+                    }
+                    
+                }
+                catch (Exception ex)
+                {
+
+                    return BadRequest(ex.Message);
+                }
+               
+            }
+        }
+
+         
+        public IHttpActionResult Delete(int id)
+        {
+            try
+            {
+                using (ABCompanyEntities entities = new ABCompanyEntities())
+                {
+                    entities.Configuration.ProxyCreationEnabled = false;
+                    var empdelete = entities.tblUsers.FirstOrDefault(p => p.ID == id);
+                    if (empdelete != null)
+                    {
+                        entities.tblUsers.Remove(empdelete);
+                        entities.SaveChanges();
+                        return Ok<string>("Record is deleted for id " + id);
+                    }
+                    else
+                    {
+                        return Ok<string>("No record in table for id " + id);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+           
+        }
+
+        public IHttpActionResult Post([FromBody] tblUser emp)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest("Data is not valid please recheck!");
+                }
+
+                using (ABCompanyEntities entities = new ABCompanyEntities())
+                {
+                    entities.tblUsers.Add(emp);
+                    var output =   entities.SaveChanges();                   
+                    if (output == 1)
+                    {
+                        return Ok<string>("Data Saved Succcessfully");
+                        
+                    }
+                    else
+                    {
+                        return Ok<string>("Data Not Saved ");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+           
+        }
+
+        public IHttpActionResult put(tblUser user)
+        {
+            try
+            {
+                using (ABCompanyEntities entities = new ABCompanyEntities())
+                {
+                    entities.Configuration.ProxyCreationEnabled = false;
+
+                    var userUpdate = entities.tblUsers.Where(s => s.ID == user.ID).FirstOrDefault<tblUser>();
+                    
+                    if (userUpdate != null)
+                    {
+                        userUpdate.Name = user.Name;
+                        userUpdate.PhoneNumber = user.PhoneNumber;
+                        userUpdate.Email = user.Email;
+                        userUpdate.Address = user.Address;
+                        entities.SaveChanges();
+                        return Ok<string>("Data Updated Succcessfully");
+                    }
+                    else
+                    {
+                        return Ok<string>("Data Not Updated ");
+                    }
+
+
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            
+        }
+
+    }
+}
